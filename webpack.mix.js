33ab4e07c8451e13670759db6ff343514209603c
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-25 12:59:37 -0500

    Agregando configuracion de laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('frontend/src/main.js', 'public/js')
+    .alias({
+        '@': path.join(__dirname, 'frontend/src')
+    })
+    .vue();
